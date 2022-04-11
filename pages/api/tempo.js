fbc7fb148847305c531f630497e539fb09c9fdc9
kesley-tech <kesley.cunha@outlook.com>
2022-04-10 23:43:46 -0300

    Atualizando tempo.js

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,17 @@
-function tempo(request, response){
+async function tempo(request, response){
+    const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
 
+    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+    const subscribersResoponseJson = await subscribersResponse.json();
+    const error = subscribersResoponseJson.error;
+
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
     response.json({
-        date: dynamicDate.toGMTString()
-    })
+        date: dynamicDate.toGMTString(),
+        error: error
+    });
 }
 
 export default tempo;
